ad0390776cd6e591e9a537066a71c7d6b4935143
Elson Junior. <167557011+ElsonJr01@users.noreply.github.com>
2025-01-12 00:59:39 -0300

    Add files via upload

---
+++ VetoresStructs.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	int dia, mes, ano;
+}DataNas;
+
+typedef struct {
+	DataNas dataNas;
+	int idade;
+	char sexo;
+	char nome[100];
+}Pessoa;
+
+void imprime(Pessoa p){
+		printf("\tNome: %s\n", p.nome);
+		printf("\tIdade: %d\n", p.idade);
+		printf("\tSexo: %c\n", p.sexo);
+		printf("\tData de Nasc: %2d/%2d/%4d\n", p.dataNas.dia, p.dataNas.mes, p.dataNas.ano);		
+}
+
+Pessoa lerpessoa(){
+	Pessoa p;
+	printf("\nDigite o nome do usuario: ");
+	fgets(p.nome, 100, stdin);
+	printf("\nDigite a idade: ");
+	scanf("%d", &p.idade);
+	scanf("%c");
+	printf("\nDigite M para Masculino e F para Feminino: ");
+	scanf("%c", &p.sexo);
+	printf("\nDigite dia, mes e ano nesse formato: dd mm aaaa: ");
+	scanf("%2d %2d %4d", &p.dataNas.dia, &p.dataNas.mes, &p.dataNas.ano);
+	scanf("%c");
+	return p;
+}
+
+int main(){
+	
+	int i;
+	Pessoa pessoas[3];
+	printf("%d\n", sizeof(Pessoa));
+	
+	for(i=0; i<3; i++){
+		pessoas[i] = lerpessoa();
+		}
+		for(i=0; i<3; i++){
+		imprime(pessoas[i]);
+ 		}
+	return 0;
+}
